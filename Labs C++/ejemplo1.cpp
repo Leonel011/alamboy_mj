738f401fd1ca69d9ec18fc2dc4a6ea24392cd92f
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-03-05 21:11:51 -0400

    "updatefile"

--- Labs C++/ejemplo1.cpp
+++ Labs C++/ejemplo1.cpp
@@ -1,10 +1,13 @@
-include <iostream>
-using namespace std;
-    int main() 
-    {
+#include <iostream>
+ using namespace std;
+ int main()
+
+{
     int myVariable = 10;
     const int myConstant = 20;
-    cout << "Valor de mi variable: " << myVariable << endl;
-    cout << "Valor de mi constante: " << myConstant << endl;
+
+    cout <<"Valor de mi variable: "<<myVariable<< endl;
+    cout <<"Valor de mi constante: "<<myConstant<<endl;
+
     return 0;
-    }
+}
