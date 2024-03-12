3f9578237f0d14711bcaba250ce4da7880a9bccc
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-03-12 10:58:20 -0300

    "updatefile"

--- LabsJava/ejemplo2/src/App.java
+++ LabsJava/ejemplo2/src/App.java
@@ -1,5 +1,22 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero entero: ");
+        int entero = scanner.nextInt();
+
+        System.out.print("Ingrese un numero flotante: ");
+        float flotante = scanner.nextFloat();
+
+        System.out.print("Ingrese un caracter: ");
+        char caracter = scanner.next() .charAt(0);
+
+        scanner.next.Line();
+
+        System.out.print("Ingrese una cadena de caracteres: ");
+        String cadena = scanner .nextLine();
     }
 }
