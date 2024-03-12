60177876640de40e4a71258e6a710292d570c1ca
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-03-12 10:14:15 -0300

    "updatefile"

---
+++ Labs C++/ejemplo2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int entero;
+    float flotante;
+    char caracter;
+    string cadena;
+
+    cout << "Ingrese un numero entero: ";
+    cin >> entero;
+
+    cout << "Ingrese un numero flotante: ";
+    cin >> flotante;
+
+    cout << "Ingrese un caracter: ";
+    cin>> caracter;
+
+    cout << "Ingrese una cadena de caracteres: ";
+    getline(cin, cadena);
+    cadena
+
+    return 0;
+}
