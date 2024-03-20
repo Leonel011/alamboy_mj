eb6442cbb770fb0205581737fda3fa7d4555cc77
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-03-20 19:29:23 -0300

    "updatefile"

--- Labs C++/distancia.cpp
+++ Labs C++/distancia.cpp
@@ -1,1 +1,22 @@
 //Desarrolla un algoritmo que calcule la distancia total recorrida en un viaje,dada la velocidad promedio y el tiempo de viaje.
+
+#include <iostream>
+ using namespace std;
+ int main()
+
+{
+    //definir variables
+    double velocidad, tiempo, distancia;
+
+    cout<<"Ingrese la velocidad promedio durante el viaje: ";
+    cin>>velocidad;
+
+    cout<<"Ingrese el tiempo que le tomo llegar: ";
+    cin>>tiempo;
+
+    distancia = velocidad * tiempo;
+
+    cout<<"La distancia recorrida fue: "<<distancia<<endl;
+    
+    return 0;
+}
