3d7aa107b9fc33cbe82615f20dd7a271e27943c2
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-03-26 09:15:46 -0300

    "updatefile"

---
+++ Labs C++/ej2.TryCatch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    float numero; //variable tipo float
+    int entero; // variable tipo entero
+
+    cout<<"Ingrese un numero: ";
+    cin>>numero;
+
+    entero = numero; // extraccion parte entera del numero entrado.
+
+    try
+    {
+        if(numero != entero)
+        {
+            throw "El numero ingresado no es un numero entero";
+        }
+        cout<<"El numero entero ingresado es: "<<numero<<endl;
+    }
+    catch(const char* mensaje)
+    {
+        cout<<"Excepcion capturada"<<endl;
+    }
+    
+    return 0;
+}
