7c5e60e94a63453c9aecdd5cc21bdb84c6c3d283
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-04-01 20:42:23 -0300

    "updatefile"

--- LabsJava/bonoEmpleados.java/src/App.java
+++ LabsJava/bonoEmpleados.java/src/App.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class App
- {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+{
+    public static void main(String[] args)
+
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter how many years you have worked in the business: ");
+        int years = scanner.nextInt();
+
+        if (years >= 5 )
+        {
+            System.out.println("Will recieve a bonus of 5000");
+        }
+
+        if (years < 5)
+        {
+            System.out.println("Will not recieve a bonus.");
+        }
     }
 }
