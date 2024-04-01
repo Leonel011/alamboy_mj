af6693ffad8fcf880b86770383b0fe69e751dbd2
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-04-01 18:11:55 -0300

    "updatefile"

--- Labs C++/estadoTarjeta.cpp
+++ Labs C++/estadoTarjeta.cpp
@@ -3,5 +3,13 @@ using namespace std;
 int main()
 
 {
-    
+    string estado;
+
+    cout<<"Ingrese si su tarjeta esta activa o inactiva: ";
+    cin>>estado;
+
+    if (estado=="activa")
+    {
+        throw "Puede realizar prestamos."
+    }
 }
