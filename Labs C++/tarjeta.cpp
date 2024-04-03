ae86a2f1a030cda8822605cd7dff3d844dc369ac
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-04-03 20:45:04 -0300

    "updatefile"

---
+++ Labs C++/tarjeta.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+
+//Desarrolle un programa que solicite al usuario si el estado de la tarjeta es activo o inactivo
+//Si es activo notifique que puede hacer prestamos, si es inactivo no podra hacer prestamos
+
+{
+    //Definir Variable
+
+    string estado;
+
+    cout<<"Ingrese si su tarjeta esta activa o inactiva: ";
+    cin>> estado;
+
+    //Determinar si es activa o inactiva
+
+    if (estado == "activa")
+
+    cout<<"Puede realizar prestamos."<<endl;
+
+    else
+
+    cout<<"No puede realizar prestamos."<<endl;
+}
