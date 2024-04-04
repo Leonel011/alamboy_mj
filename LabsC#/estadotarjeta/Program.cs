0d2ecaf5be279e907ae633497d426e8dab496379
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-04-03 21:29:15 -0300

    "updatefile"

--- LabsC#/estadotarjeta/Program.cs
+++ LabsC#/estadotarjeta/Program.cs
@@ -1,1 +1,18 @@
-﻿//
+﻿//Desarrolle un programa que permita determinar si el usuario puede hacer prestamos de libros dependiendo del estado de su tarjeta.
+//Que el usuario notifique si su tarjeta esta activa o inactiva
+
+using System;
+namespace EstadoTarjeta
+
+{
+    class Program 
+    {
+        static void Main(string[]args)
+
+        {
+            Console.Write("Ingrese el estado de la tarjeta como activa o inactiva: ");
+
+            string estado = string.Parse(Console.ReadLine());
+        }
+    }
+}
