986a304a39099666fb9108cd94d635f48339c4e6
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-05-06 18:48:25 -0300

    "updatefile"

---
+++ LabsJava/nombre/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args) 
+    {
+
+        String nombre = "Adriel";
+
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println("Hola "+ nombre);
+        }
+    }
+}
