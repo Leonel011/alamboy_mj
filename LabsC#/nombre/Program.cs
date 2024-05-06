19b58388e6b12af4be35a4e8ad9bce39ccaf225b
Adriel Lamboy <lamboy.adriel@gmail.com>
2024-05-06 20:41:48 -0300

    "updatefile"

---
+++ LabsC#/nombre/Program.cs
@@ -0,0 +1,18 @@
+﻿//Desarrolle un programa que imprima diez veces la palabra “Hola ” y su nombre
+
+using System;
+namespace nombre
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        string nombre = "Adriel Lamboy";
+    
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hola" + nombre);
+        }
+    }
+}
